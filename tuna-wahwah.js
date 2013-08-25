8078ebe6434ec56b02f83e942125b5e54d0ef543
janesconference <janesconference@bitterspring.net>
2013-08-25 01:52:24 +0100

    enabling load/save on wah plugin

--- tuna-wahwah.js
+++ tuna-wahwah.js
@@ -27,16 +27,25 @@ define(['require', 'kievII', 'tuna-amd', 'image'], function(require, K2, Tuna) {
         var switchDownImage =  resources[3];
         
         var tuna = new Tuna(this.context);
+
+        if (args.initialState && args.initialState.data) {
+            /* Load data */
+            this.pluginState = args.initialState.data;
+        }
+        else {
+            /* Use default data */
+            this.pluginState = {
+                automode: true,                //true/false
+                baseFrequency: 0.5,            //0 to 1
+                excursionOctaves: 2,           //1 to 6
+                sweep: 0.2,                    //0 to 1
+                resonance: 10,                 //1 to 100
+                sensitivity: 0.5,              //-1 to 1
+                bypass: 0
+            };
+        }
         
-        this.wahwah = new tuna.WahWah({
-                 automode: true,                //true/false
-                 baseFrequency: 0.5,            //0 to 1
-                 excursionOctaves: 2,           //1 to 6
-                 sweep: 0.2,                    //0 to 1
-                 resonance: 10,                 //1 to 100
-                 sensitivity: 0.5,              //-1 to 1
-                 bypass: 0
-             });
+        this.wahwah = new tuna.WahWah(this.pluginState);
     
        this.audioSource.connect(this.wahwah.input);
        this.wahwah.connect(this.audioDestination);
@@ -50,11 +59,11 @@ define(['require', 'kievII', 'tuna-amd', 'image'], function(require, K2, Tuna) {
        var initOffset = 22;
        var knobSpacing = 83;
        var knobTop = 20;
-       this.knobDescription = [ {id: 'baseFrequency', init: 0.5, range: [0,1]},
-                                {id: 'excursionOctaves', init: 2, range: [1,6]},
-                                {id: 'sweep', init: 0.2, range: [0,1]},
-                                {id: 'resonance', init: 10, range: [1,100]},
-                                {id: 'sensitivity', init: 0.5, range: [-1,1]}
+       this.knobDescription = [ {id: 'baseFrequency', init: this.pluginState.baseFrequency, range: [0,1]},
+                                {id: 'excursionOctaves', init: this.pluginState.excursionOctaves, range: [1,6]},
+                                {id: 'sweep', init: this.pluginState.sweep, range: [0,1]},
+                                {id: 'resonance', init: this.pluginState.resonance, range: [1,100]},
+                                {id: 'sensitivity', init: this.pluginState.sensitivity, range: [-1,1]}
                               ];
 
         /* deck */
@@ -92,7 +101,7 @@ define(['require', 'kievII', 'tuna-amd', 'image'], function(require, K2, Tuna) {
                 if (knobElIndex !== -1) {
                     var setValue = K2.MathUtils.linearRange (0, 1, currKnob.range[0], currKnob.range[1], value);
                     console.log ("Setting", value, setValue, "to", element);
-                    this.wahwah[element] =  setValue;
+                    this.wahwah[element] = this.pluginState[element] = setValue;
                 }
                 else {
                     console.error ("element index invalid:",  knobElIndex);
@@ -119,10 +128,10 @@ define(['require', 'kievII', 'tuna-amd', 'image'], function(require, K2, Tuna) {
             imagesArray : [switchDownImage, switchUpImage],
             onValueSet: function (slot, value) {
                 if (value === 1) {
-                    this.wahwah.automode =  true;
+                    this.wahwah.automode = this.pluginState.automode = true;
                 }
                 else {
-                    this.wahwah.automode =  false;
+                    this.wahwah.automode = this.pluginState.automode = false;
                 }
                 this.ui.refresh();
             }.bind(this),
@@ -130,9 +139,20 @@ define(['require', 'kievII', 'tuna-amd', 'image'], function(require, K2, Tuna) {
         };
         
         this.ui.addElement(new K2.Button(buttonArgs));
-        this.ui.setValue ({elementID: buttonArgs.ID, value: 1});
+
+        if (this.wahwah.automode) {
+            this.ui.setValue ({elementID: buttonArgs.ID, value: 1});
+        }
+        else {
+            this.ui.setValue ({elementID: buttonArgs.ID, value: 0});
+        }
        
         this.ui.refresh();
+
+        var saveState = function () {
+            return { data: this.pluginState };
+        };
+        args.hostInterface.setSaveState (saveState);
 
         // Initialization made it so far: plugin is ready.
         args.hostInterface.setInstanceStatus ('ready');
