4c21b5871a3c290044c1c6b76dd49816334d3efe
Cristiano Belloni <janesconference@bitterspring.net>
2013-09-27 17:03:58 +0100

    package.json, minified images, K2 0.6.0

--- tuna-phaser.js
+++ tuna-phaser.js
@@ -1,4 +1,4 @@
-define(['require', 'github:janesconference/KievII@jspm0.5/dist/kievII',
+define(['require', 'github:janesconference/KievII@0.6.0/kievII',
     'github:janesconference/tuna/tuna'], function(require, K2, Tuna) {
   
     var pluginConf = {
