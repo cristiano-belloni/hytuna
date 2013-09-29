76e0fc81d2cd2ed9788fc0369cbdd46fb40a0250
janesconference <janesconference@bitterspring.net>
2013-09-29 18:20:57 +0100

    Using tuna / master

---
+++ tuna-convolver.js
@@ -0,0 +1,155 @@
+define(['require', 'github:janesconference/KievII@0.6.0/kievII',
+    'github:janesconference/tuna/tuna'], function(require, K2, Tuna) {
+  
+    var pluginConf = {
+        name: "Tuna Convolver",
+        osc: false,
+        audioOut: 1,
+        audioIn: 1,
+        version: '0.0.1-alpha1',
+        ui: {
+            type: 'canvas',
+            width: 440,
+            height: 282
+        }
+    };
+
+    var pluginFunction = function(args, resources) {
+        
+        this.id = args.id;
+        this.audioSource = args.audioSources[0];
+        this.audioDestination = args.audioDestinations[0];
+        this.context = args.audioContext;
+        
+        var knobImage =  resources[0];
+        var deckImage =  resources[1];
+
+        if (args.initialState && args.initialState.data) {
+            /* Load data */
+            this.pluginState = args.initialState.data;
+        }
+        else {
+            /* Use default data */
+            this.pluginState = {
+                highCut: 22050,                         //20 to 22050
+                lowCut: 20,                             //20 to 22050
+                dryLevel: 1,                            //0 to 1+
+                wetLevel: 1,                            //0 to 1+
+                level: 1,                               //0 to 1+, adjusts total output of both wet and dry
+                decodedImpulse: null,
+                bypass: 0
+            };
+        }
+        
+        var tuna = new Tuna(this.context);
+        
+        this.convo = new tuna.Convolver(this.pluginState);
+        
+    
+        this.audioSource.connect(this.convo.input);
+        this.convo.connect(this.audioDestination);
+       
+        // The canvas part
+        this.ui = new K2.UI ({type: 'CANVAS2D', target: args.canvas});
+
+        var initOffset = 21;
+        var knobSpacing = 83;
+        var knobTop = 184;
+        this.knobDescription = [ {id: 'highCut', init: this.pluginState.highCut, range: [20,22050]},
+            {id: 'lowCut', init: this.pluginState.lowCut, range: [20,22050]},
+            {id: 'wetLevel', init: this.pluginState.wetLevel, range: [0,1]},
+            {id: 'dryLevel', init: this.pluginState.dryLevel, range: [0,1]},
+            {id: 'level', init: this.pluginState.level, range: [0,1]}
+        ];
+
+        /* deck */
+        var bgArgs = new K2.Background({
+            ID: 'background',
+            image: deckImage,
+            top: 0,
+            left: 0
+        });
+    
+        this.ui.addElement(bgArgs, {zIndex: 0});
+        
+        /* knobs */
+        var knobArgs = {
+             ID: "",
+             left: 0,
+             top: knobTop,
+             sensitivity : 5000,
+             tileWidth: 64,
+             tileHeight: 64,
+             imageNum: 64,
+             imagesArray: [knobImage],
+             bottomAngularOffset: 33,
+             onValueSet: function (slot, value, element) {
+                //Find the id
+                var knobElIndex = -1;
+                var currKnob;
+                for (var i = 0; i < this.knobDescription.length; i+=1) {
+                    currKnob = this.knobDescription[i];
+                    if (currKnob.id === element) {
+                        knobElIndex = i;
+                        break;
+                    }
+                }
+                if (knobElIndex !== -1) {
+                    var setValue = K2.MathUtils.linearRange (0, 1, currKnob.range[0], currKnob.range[1], value);
+                    console.log ("Setting", value, setValue, "to", element);
+                    this.convo[element] = this.pluginState[element] = setValue;
+                }
+                else {
+                    console.error ("element index invalid:",  knobElIndex);
+                }
+                
+                this.ui.refresh();
+             }.bind(this)
+         };
+         
+         for (var i = 0; i < this.knobDescription.length; i+=1) {
+             var currKnob = this.knobDescription[i];
+             knobArgs.ID = currKnob.id;
+             knobArgs.left = (initOffset + i * knobSpacing);
+             this.ui.addElement(new K2.Knob(knobArgs));
+             var initValue = K2.MathUtils.linearRange (currKnob.range[0], currKnob.range[1], 0, 1, currKnob.init);
+             this.ui.setValue ({elementID: knobArgs.ID, value: initValue});
+        }
+       
+        this.ui.refresh();
+
+        var saveState = function () {
+            return { data: this.pluginState };
+        };
+        args.hostInterface.setSaveState (saveState);
+
+        // Initialization made it so far: plugin is ready.
+        args.hostInterface.setInstanceStatus ('ready');
+    };
+    
+    
+    var initPlugin = function(initArgs) {
+        var args = initArgs;
+
+        var requireErr = function (err) {
+            var failedId = err.requireModules && err.requireModules[0];
+            requirejs.undef(failedId);
+            args.hostInterface.setInstanceStatus ('fatal', {description: 'Error initializing plugin: ' + failedId});
+        }.bind(this);
+        
+        require (['./assets/images/knob_64_64_64.png!image',
+                  './assets/images/TCoDeck.png!image'],
+            function () {
+                var resources = arguments;
+                pluginFunction.call (this, args, resources);
+            }.bind(this),
+            requireErr
+        );
+
+    };
+        
+    return {
+        initPlugin: initPlugin,
+        pluginConf: pluginConf
+    };
+});
